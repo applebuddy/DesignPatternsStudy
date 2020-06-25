4d905f69224750599c1c7d9608f89fd831b8524b
MinKyeongTae <kyeongtea92@gmail.com>
2020-06-25 20:36:22 +0900

    Study about MVVM; What should you be careful about?

--- Contents.swift
+++ Contents.swift
@@ -22,10 +22,7 @@ public class Pet {
     public let rarity: Rarity
     public let image: UIImage
 
-    public init(name: String,
-                birthday: Date,
-                rarity: Rarity,
-                image: UIImage) {
+    public init(name: String, birthday: Date, rarity: Rarity, image: UIImage) {
         self.name = name
         self.birthday = birthday
         self.rarity = rarity
@@ -40,7 +37,7 @@ public class Pet {
 // MARK: ViewModel
 
 public class PetViewModel {
-    // 1) pet
+    // 1) pet : Pet 객체를 생성합니다. 나이 연산을 위해 Calendar도 생성합니다.
     private let pet: Pet
     private let calendar: Calendar
 
@@ -49,16 +46,17 @@ public class PetViewModel {
         calendar = Calendar(identifier: .gregorian)
     }
 
-    // 2) name
+    // 2) name : 펫의 이름을 반환합니다.
     public var name: String {
         return pet.name
     }
 
+    // image : 펫의 이미지를 반환합니다.
     public var image: UIImage {
         return pet.image
     }
 
-    // 3) ageText
+    // 3) ageText : 펫의 나이를 연산하며 반환합니다. 몇살인지를 표출합니다.
     public var ageText: String {
         let today = calendar.startOfDay(for: Date())
         let birthday = calendar.startOfDay(for: pet.birthday)
@@ -67,7 +65,7 @@ public class PetViewModel {
         return "\(age) years old"
     }
 
-    // 4) adoptionFeeText
+    // 4) adoptionFeeText : 해당 펫의 희귀성에 따른 가격을 결정합니다.
     public var adoptionFeeText: String {
         switch pet.rarity {
         case .common:
@@ -146,13 +144,13 @@ public class PetView: UIView {
 // 1
 let birthday = Date(timeIntervalSinceNow: -2 * 85400 * 366)
 let image = UIImage(named: "stuart")!
-let stuart = Pet(name: "Stuart",
+let mungGu = Pet(name: "mungGu",
                  birthday: birthday,
                  rarity: .veryRare,
                  image: image)
 
 // 2
-let viewModel = PetViewModel(pet: stuart)
+let viewModel = PetViewModel(pet: mungGu)
 
 // 3
 let frame = CGRect(x: 0, y: 0, width: 300, height: 420)
@@ -161,13 +159,13 @@ let view = PetView(frame: frame)
 // 4
 
 /*
- // 1) viewModel extension -> configure 메서드 미사용 시
+ // 4-1) viewModel extension -> configure 메서드 미사용 시
  view.nameLabel.text = viewModel.name
  view.imageView.image = viewModel.image
  view.ageLabel.text = viewModel.ageText
  view.adoptionFeeLabel.text = viewModel.adoptionFeeText
  */
-// 2) viewModel extension -> configure 메서드 사용 시
+// 4-2) viewModel extension -> configure 메서드 사용 시
 viewModel.configure(view)
 
 // 5
@@ -197,3 +195,23 @@ extension PetViewModel {
 }
 
 // MARK: - MMVM을 구현할 때 무엇을 주의해야 할까요?
+
+// var S = "20200523"
+// let index = S.startIndex
+// S.insert("-", at: S.index(index, offsetBy: 4))
+// S.insert("-", at: S.index(index, offsetBy: 7))
+// print(S)
+
+var S = [1, 2, 3, 4, 5]
+
+// MARK: MVVM 패턴 사용 간 주의해야 할 사항
+
+// What should you be careful about?
+// MVVM 사용 시 무엇을 주의해야 할까?
+
+// 만약 많은 Model-to-View 변환을 요구하는 경우에 MVVM는 유용하게 사용될 수 있습니다.
+// 하지만, 모든 객체들이 모델, 뷰, 뷰모델의 범주에 항상 딱 맞지는 않을 것입니다., 이때는 대신 다른 디자인 패턴들과 함께 조합을 해서 MVVM을 사용해야만 합니다.
+//
+// 무엇보다도, 처음으로 앱을 만들어 본다면, MVVM은 매우 유용하지 못할 수 있습니다. MVC는 개발의 시작점으로 좋은 패턴입니다. MVC(Model-View-Controller) 디자인패턴은 Cocoa에서 기반으로 사용중인 패턴일 뿐만 아니라 다른 패턴에 비해 비교적 간단하게 구현할 수 있는 장점이 있기 때문입니다. 앱의 요구사장일 변경될 때, 당신은 이러한 변화된 요구사항에 맞게 다른 디자인 패턴을 선택해야할 필요를 느낄 수 있습니다. 이런 상황이 오게 된다면, 앱 내에 MVVM을 적용해보는 것은 좋습니다.
+//
+// 그때 가서 MVVM 패턴을 사용하는 것에 겁먹지 마세요. 대신 미리 준비 하세요.
